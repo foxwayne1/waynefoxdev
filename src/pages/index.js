d3e2d31144476d9e2642bd9ebc60a32cb754ea04
foxwayne1 <fox.wayne1@gmail.com>
2022-08-18 13:28:21 +0100

    added google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import { Element } from 'react-scroll'
+import React from "react";
+import { Element } from "react-scroll";
 
-import PageWrapper from '../components/PageWrapper'
-import Hero from '../sections/landing3/Hero'
-import Works from '../sections/landing3/Works'
-import Contact from '../sections/landing3/Contact'
+import PageWrapper from "../components/PageWrapper";
+import Hero from "../sections/landing3/Hero";
+import Works from "../sections/landing3/Works";
+import Contact from "../sections/landing3/Contact";
 
 const IndexPage = () => {
   return (
     <>
+      {/* GOOGLE TAGS FOR ANALYTICS */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-XVSHXZ255H"
+      ></script>
+      <script>
+        {`window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-XVSHXZ255H');`}
+      </script>
+
       <PageWrapper>
         <Hero />
-        <Element name='works'>
+        <Element name="works">
           <Works />
         </Element>
 
         <Contact />
       </PageWrapper>
     </>
-  )
-}
-export default IndexPage
+  );
+};
+export default IndexPage;
