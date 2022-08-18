e17f39c59cbc048f3e5b1375522b8d4d500b7554
foxwayne1 <fox.wayne1@gmail.com>
2022-08-18 11:18:48 +0100

    update new portfoltio items

--- src/data/devWorks1.js
+++ src/data/devWorks1.js
@@ -1,15 +1,80 @@
-import imgT1 from '../assets/image/jpg/details/order-summary-component-1.jpg'
-import imgT2 from '../assets/image/jpg/details/profile-card-component-1.jpg'
-import imgT3 from '../assets/image/jpg/details/qr-code-component-1.jpg'
+import imgT1 from "../assets/image/jpg/details/order-summary-component-1.jpg";
+import imgT2 from "../assets/image/jpg/details/profile-card-component-1.jpg";
+import imgT3 from "../assets/image/jpg/details/qr-code-component-1.jpg";
+import imgT4 from "../assets/image/jpg/details/manage-main.png";
+import imgT5 from "../assets/image/jpg/details/guess-my-number-main.png";
+import imgT6 from "../assets/image/jpg/details/panellum-main.png";
+import imgT7 from "../assets/image/jpg/details/reviews-main.png";
+import imgT8 from "../assets/image/jpg/details/tappit-main.png";
+import imgT9 from "../assets/image/jpg/details/tours-main.png";
+import imgT10 from "../assets/image/jpg/details/birthday-main.png";
 
 export const devWorks1 = [
+  {
+    brand: `Birthday Reminder`,
+    slug: `birthdays`,
+    title: `A React JS based Birthday Reminder.`,
+    categories: [`html`, `css`, `react`],
+    thumbnail: imgT10,
+    link: "/birthdays",
+  },
+  {
+    brand: `Tours`,
+    slug: `tours`,
+    title: `A React JS based Tours website loading data from API.`,
+    categories: [`html`, `css`, `react`],
+    thumbnail: imgT9,
+    link: "/tours",
+  },
+  {
+    brand: `Tappit`,
+    slug: `tappit`,
+    title: `A React JS based company challenge to build a table with
+    pagination.`,
+    categories: [`html`, `css`, `react`, `tailwind`, `pagination`],
+    thumbnail: imgT8,
+    link: "/tappit",
+  },
+  {
+    brand: `React Reviews`,
+    slug: `reviews`,
+    title: `A React JS based Review page.`,
+    categories: [`html`, `css`, `react`],
+    thumbnail: imgT7,
+    link: "/reviews",
+  },
+  {
+    brand: `Pannellum JS`,
+    slug: `pannellum`,
+    title: `Using Pannellum JS to build a basic setup for a company
+    developer test.`,
+    categories: [`html`, `css`, `javascript`, `pannellum js`],
+    thumbnail: imgT6,
+    link: "/pannellum",
+  },
+  {
+    brand: `Guess My Number`,
+    slug: `guess-my-number`,
+    title: `A JavaScript based guess number game.`,
+    categories: [`html`, `css`, `javascript`],
+    thumbnail: imgT5,
+    link: "/guess-my-number",
+  },
+  {
+    brand: `Manage Landing Page`,
+    slug: `manage-landing-page`,
+    title: `A perfect project for newbies who are starting to build confidence with layouts!`,
+    categories: [`html`, `tailwind`, "css"],
+    thumbnail: imgT4,
+    link: "/manage-landing-page",
+  },
   {
     brand: `Order Summary Component`,
     slug: `order-summary-component`,
     title: `A perfect project for newbies who are starting to build confidence with layouts!`,
     categories: [`html`, `css`],
     thumbnail: imgT1,
-    link: '/order-summary-component',
+    link: "/order-summary-component",
   },
   {
     brand: `Profile card component`,
@@ -17,7 +82,7 @@ export const devWorks1 = [
     title: `This is a perfect challenge to test your layout skills. The card layout doesn't shift, so it's also great for those that haven't dived into responsive websites yet!`,
     categories: [`html`, `css`],
     thumbnail: imgT2,
-    link: '/profile-card-component',
+    link: "/profile-card-component",
   },
   {
     brand: `QR code component`,
@@ -25,6 +90,6 @@ export const devWorks1 = [
     title: `Adaptable but Identifiable Product for Kandinsky.`,
     categories: [`html`, `css`],
     thumbnail: imgT3,
-    link: '/qr-code-component',
+    link: "/qr-code-component",
   },
-]
+];
---
+++ src/pages/pannellum.js
@@ -0,0 +1,137 @@
+import React from "react";
+import Link from "next/link";
+import { Container, Row, Col } from "react-bootstrap";
+
+import { Section, Button, Title, Text, Box } from "../components/Core";
+import PageWrapper from "../components/PageWrapper";
+import CTA from "../sections/common/CTA";
+import imgWorkCover from "../assets/image/jpg/details/panellum-main.png";
+import imgS1 from "../assets/image/jpg/details/panellum-github.png";
+import imgS2 from "../assets/image/jpg/details/details-11.jpg";
+import imgS3 from "../assets/image/jpg/details/details-10.jpg";
+import imgS4 from "../assets/image/jpg/details/details-9.jpg";
+import imgS5 from "../assets/image/jpg/details/details-14.jpg";
+import imgS6 from "../assets/image/jpg/details/details-13.jpg";
+import { device } from "../utils";
+
+const WorkSingle = () => {
+  return (
+    <>
+      <PageWrapper lightTheme>
+        <Section hero>
+          <Container>
+            <Row className="justify-content-center text-center">
+              <Col lg="8">
+                <Box>
+                  <Text variant="tag">HTML, CSS, JAVASCRIPT, PANNELLUM JS</Text>
+                  <Title className="my-4">Pannellum JS</Title>
+                  <Text
+                    variant="p"
+                    css={`
+                      max-width: 750px;
+                    `}
+                  >
+                    Using Pannellum JS to build a basic setup for a company
+                    developer test.
+                  </Text>
+                </Box>
+              </Col>
+            </Row>
+
+            <Box pt={["4rem", null, null, "6.25rem"]}>
+              <img src={imgWorkCover} alt="" className="img-fluid w-100" />
+            </Box>
+          </Container>
+        </Section>
+
+        <div className="mt-lg-3">
+          <Container>
+            <Row>
+              <Col lg="4" className="mb-4 mb-lg-0">
+                <Link href="https://github.com/foxwayne1/Pannellum-test-streamGo">
+                  <a target="_blank">
+                    <Button arrowRight>Github Repo</Button>
+                  </a>
+                </Link>
+              </Col>
+              <Col lg="4" className="mb-4 mb-lg-0">
+                <Link href="https://panellum-test-streamgo.netlify.app/">
+                  <a target="_blank">
+                    <Button arrowRight>Deployed Site</Button>
+                  </a>
+                </Link>
+              </Col>
+              <Col lg="4" className="mb-4 mb-lg-0">
+                <Link href="https://pannellum.org/">
+                  <a target="_blank">
+                    <Button arrowRight>Pannellum JS</Button>
+                  </a>
+                </Link>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <Section className="mt-lg-5 pb-0">
+          <Container>
+            <Row>
+              <Col xs="12" className="mb-5">
+                <img src={imgS1} alt="" className="img-fluid w-100" />
+              </Col>
+              {/* <Col xs='12' className='mb-5'>
+                <img src={imgS2} alt='' className='img-fluid w-100' />
+              </Col>
+              <Col lg='6' className='mb-5 mb-lg-0 pr-lg-4'>
+                <img src={imgS3} alt='' className='img-fluid w-100' />
+              </Col>
+              <Col lg='6' className='mb-5 pl-lg-4'>
+                <img src={imgS4} alt='' className='img-fluid w-100' />
+              </Col> */}
+            </Row>
+          </Container>
+        </Section>
+
+        {/* <Section bg='dark' className='pt-0'>
+          <Container>
+            <Row>
+              <Col xs='12' className='mb-5'>
+                <img src={imgS5} alt='' className='img-fluid w-100' />
+              </Col>
+              <Col xs='12' className='mb-5'>
+                <img src={imgS6} alt='' className='img-fluid w-100' />
+              </Col>
+            </Row>
+
+            <div className='text-center mt-lg-5'>
+              <Text variant='tag' className='mb-1' color='lightShade'>
+                Next Project
+              </Text>
+              <Link href='/portfolio-details'>
+                <a>
+                  <Button
+                    arrowRight
+                    className='border-0 bg-transparent shadow-none text-capitalize py-3'
+                    css={`
+                    font-weight: 700
+                    font-size: 1.5rem;
+                    letter-spacing: -1.2px;
+                    line-height: 1.375;
+                    @media ${device.md} {
+                      font-size: 2rem;
+                    }
+                `}
+                  >
+                    Replace
+                  </Button>
+                </a>
+              </Link>
+            </div>
+          </Container>
+        </Section> */}
+        <Box py={4}>
+          <CTA />
+        </Box>
+      </PageWrapper>
+    </>
+  );
+};
+export default WorkSingle;
